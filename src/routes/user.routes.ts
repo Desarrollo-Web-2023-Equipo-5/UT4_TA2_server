672cab0885fbd658f4efb9fa903bfd119007ed74
Ikken9 <pierosaucedo3@gmail.com>
2023-06-07 12:40:10 -0300

    Implemented 'addTask'

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { createUser, getUserById } from '../controllers/user.controller';
+import { createUser, getUserById, addTask } from '../controllers/user.controller';
 
 const router = Router();
 
 router.post('/users', createUser);
 router.get('/users/:id', getUserById);
+
+router.post('/users/:id/tasks', addTask);
 
 export default router;
