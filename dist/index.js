3995e2ae1268610557208c77bb0077e723d39392
Rodrigo Luque <rodrigoluquepuig@gmail.com>
2023-06-21 20:46:29 -0300

    more tasks added

--- dist/index.js
+++ dist/index.js
@@ -5,8 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const user_routes_1 = __importDefault(require("./routes/user.routes"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const PORT = 3000;
+app.use((0, cors_1.default)()); // Middleware to enable CORS
 app.use(express_1.default.json()); // Middleware to parse the body of the request message
 app.use(express_1.default.urlencoded({ extended: false })); // Middleware to parse the body of the request message
 app.use(express_1.default.static('public')); // Middleware to serve static files
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -13,6 +13,18 @@ export const USERS: User[] = [
                 description: "Description 1",
                 done: false,
             },
+            {
+                id: "2",
+                title: "Task 2",
+                description: "Description 2",
+                done: false,
+            },
+            {
+                id: "3",
+                title: "Task 3",
+                description: "Description 3",
+                done: false,
+            },
         ]
 
     },
